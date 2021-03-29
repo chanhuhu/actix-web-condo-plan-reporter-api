859a43e0ad8c089e883e783ad987d9b0aa4c8b12
Aphimuk Yimsukpaitoon <luroman6@gmail.com>
2021-03-29 10:55:23 +0700

    fix: name when we inserting floor plan

--- src/routes/floor_plans.rs
+++ src/routes/floor_plans.rs
@@ -1,7 +1,7 @@
 use actix_multipart::Multipart;
 use actix_web::{web, Error, HttpResponse};
 use futures::TryStreamExt;
-use sqlx::types::chrono::Utc;
+use sqlx::types::chrono::{DateTime, Utc};
 use sqlx::types::Uuid;
 use sqlx::PgPool;
 use std::io::Write;
@@ -13,13 +13,28 @@ pub struct Parameters {
     project_id: String,
 }
 
+#[derive(serde::Deserialize)]
 pub struct NewFloorPlan {
     pub id: Uuid,
     pub name: String,
     pub image_url: String,
 }
+
+#[derive(sqlx::FromRow, Debug, Clone, serde::Serialize)]
+pub struct FloorPlan {
+    pub id: Uuid,
+    pub name: String,
+    pub image_url: String,
+    pub created_at: DateTime<Utc>,
+    pub updated_at: DateTime<Utc>,
+}
+
 fn get_extension_from_filename(filename: &str) -> Option<&str> {
     Path::new(filename).extension().and_then(|s| s.to_str())
+}
+
+fn get_stem_from_filename(filename: &str) -> Option<&str> {
+    Path::new(filename).file_stem().and_then(|s| s.to_str())
 }
 
 pub async fn create_floor_plan(
@@ -38,11 +53,12 @@ pub async fn create_floor_plan(
             .ok_or_else(|| HttpResponse::BadRequest().finish())?;
         let filename = sanitize_filename::sanitize(&filename);
         let file_extension = get_extension_from_filename(&filename).unwrap();
+        let file_stem = get_stem_from_filename(filename.as_ref()).unwrap();
         let floor_plan_id = Uuid::new_v4();
         let filepath = format!("./static/{}.{}", floor_plan_id, file_extension);
         let new_flor_plan = NewFloorPlan {
             id: floor_plan_id,
-            name: filename.to_string(),
+            name: file_stem.to_string(),
             image_url: format!("{}/static/{}", "http://localhost:8000", filepath),
         };
         let project_id =
@@ -67,6 +83,63 @@ pub async fn create_floor_plan(
         }
     }
     Ok(HttpResponse::Ok().into())
+}
+
+pub async fn list_floor_plans(
+    pool: web::Data<PgPool>,
+    parameters: web::Path<Parameters>,
+) -> Result<HttpResponse, HttpResponse> {
+    let project_id = Uuid::parse_str(parameters.project_id.as_ref()).unwrap();
+    let floor_plans = find_floor_plans_by_project_id(&pool, project_id)
+        .await
+        .map_err(|_| HttpResponse::InternalServerError().finish())?;
+    Ok(HttpResponse::Ok().json(floor_plans))
+}
+
+pub async fn get_floor_plan_details(
+    pool: web::Data<PgPool>,
+    floor_plan_id: web::Path<String>,
+) -> Result<HttpResponse, HttpResponse> {
+    let floor_plan_id = Uuid::parse_str(&floor_plan_id).unwrap();
+    let floor_plan = find_floor_plan(&pool, floor_plan_id)
+        .await
+        .map_err(|_| HttpResponse::InternalServerError().finish())?;
+    match floor_plan {
+        Some(res) => Ok(HttpResponse::Ok().json(res)),
+        None => Ok(HttpResponse::NotFound().finish()),
+    }
+}
+
+async fn find_floor_plan(
+    pool: &PgPool,
+    floor_plan_id: Uuid,
+) -> Result<Option<FloorPlan>, sqlx::Error> {
+    let floor_plan = sqlx::query_as::<_, FloorPlan>(r#"SELECT * FROM floor_plans WHERE id = $1"#)
+        .bind(floor_plan_id)
+        .fetch_optional(pool)
+        .await
+        .map_err(|e| {
+            log::error!("Failed to query {:?}", e);
+            e
+        })?;
+    Ok(floor_plan)
+}
+
+async fn find_floor_plans_by_project_id(
+    pool: &PgPool,
+    project_id: Uuid,
+) -> Result<Vec<FloorPlan>, sqlx::Error> {
+    log::info!("Getting projects in the database");
+    let floor_plans =
+        sqlx::query_as::<_, FloorPlan>(r#"SELECT * FROM floor_plans WHERE project_id = $1"#)
+            .bind(project_id)
+            .fetch_all(pool)
+            .await
+            .map_err(|e| {
+                log::error!("Failed to query {:?}", e);
+                e
+            })?;
+    Ok(floor_plans)
 }
 
 pub async fn insert_floor_plan(
--- src/startup.rs
+++ src/startup.rs
@@ -1,6 +1,6 @@
 use crate::routes::{
-    create_floor_plan, create_project, get_project_details, health_check, list_projects,
-    rename_project,
+    create_floor_plan, create_project, get_floor_plan_details, get_project_details, health_check,
+    list_floor_plans, list_projects, rename_project,
 };
 use actix_files::Files;
 use actix_web::dev::Server;
@@ -17,26 +17,34 @@ pub fn run(listener: TcpListener, db_pool: PgPool) -> Result<Server, std::io::Er
             .wrap(Logger::default())
             .route("/health_check", web::get().to(health_check))
             .service(
-                web::scope("/api/v1").service(
-                    web::scope("/projects")
-                        .service(
-                            web::resource("")
-                                .route(web::get().to(list_projects))
-                                .route(web::post().to(create_project)),
-                        )
-                        .service(
-                            web::scope("/{project_id}")
-                                .service(
-                                    web::resource("")
-                                        .route(web::get().to(get_project_details))
-                                        .route(web::put().to(rename_project)),
-                                )
-                                .service(
-                                    web::resource("floor_plans")
-                                        .route(web::post().to(create_floor_plan)),
-                                ),
+                web::scope("/api/v1")
+                    .service(
+                        web::scope("/projects")
+                            .service(
+                                web::resource("")
+                                    .route(web::get().to(list_projects))
+                                    .route(web::post().to(create_project)),
+                            )
+                            .service(
+                                web::scope("/{project_id}")
+                                    .service(
+                                        web::resource("")
+                                            .route(web::get().to(get_project_details))
+                                            .route(web::put().to(rename_project)),
+                                    )
+                                    .service(
+                                        web::resource("floor_plans")
+                                            .route(web::post().to(create_floor_plan))
+                                            .route(web::get().to(list_floor_plans)),
+                                    ),
+                            ),
+                    )
+                    .service(
+                        web::scope("/floor_plans").service(
+                            web::resource("/{floor_plan_id}")
+                                .route(web::get().to(get_floor_plan_details)),
                         ),
-                ),
+                    ),
             )
             .service(Files::new("/static", "./static").show_files_listing())
             .app_data(db_pool.clone())
